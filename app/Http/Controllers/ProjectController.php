87ac42b2839e35cba8737e6036f925ff1c71d3be
dtvn-Qnhat358 <nhat.tran2@dac-datatech.vn>
2022-09-26 16:56:45 +0700

    fix api edit

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -88,7 +88,7 @@ class ProjectController extends Controller
             'accountId' => 'required|integer',
             'id' => 'required|integer',
         ]);
-        $projectList = $this->projects->update($request->projectName, $request->accountId, $request->id);
+        $projectList = $this->projects->edit($request->projectName, $request->accountId, $request->id);
         return 'Update OK';
     }
 
